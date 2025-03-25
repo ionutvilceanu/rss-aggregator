6a3361b793dcc78e0f02eb854087f1fb0f182453
AzureAD\IonutVilceanu <ionut.vilceanu@empact.app>
2025-03-25 16:57:31 +0200

    refresh images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
+      // Domenii internaționale
       'www.gazzetta.it',
       'e00-marca.uecdn.es',
       'www.mundodeportivo.com',
@@ -15,10 +16,43 @@ const nextConfig = {
       'media.tenor.com',
       'phantom-marca.unidadeditorial.es',
       'static.eldiario.es',
+      
+      // Domenii românești de știri sportive
       'www.prosport.ro',
+      'prosport.ro',
       'www.gsp.ro',
+      'gsp.ro',
+      'media.gsp.ro',
+      'static.gsp.ro',
       'www.digisport.ro',
-      'www.sport.ro'
+      'digisport.ro',
+      'media.digisport.ro',
+      'www.sport.ro',
+      'sport.ro',
+      'static.sport.ro',
+      
+      // Domenii pentru servicii de imagini
+      'i.imgur.com',
+      'imgur.com',
+      'cloudinary.com',
+      'res.cloudinary.com',
+      'upload.wikimedia.org',
+      'images.unsplash.com',
+      'unsplash.com',
+      
+      // Adăugați domeniul serverului vostru de hosting aici
+      'vercel.app',
+      'localhost',
+      
+      // Pentru a permite orice domeniu (ATENȚIE: folosiți doar în dezvoltare sau pentru testare)
+      // '*',
+    ],
+    // Opțional: Permite URL-uri de imagini generate dinamic
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Permite orice subdomeniu
+      },
     ],
   },
 }
