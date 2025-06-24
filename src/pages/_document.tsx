eab43b1ac745eba6b590d91371338d566d99e68b
AzureAD\IonutVilceanu <ionut.vilceanu@empact.app>
2025-06-24 16:49:55 +0300

    add adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,11 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6587588148815143"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <body>
         <Main />
