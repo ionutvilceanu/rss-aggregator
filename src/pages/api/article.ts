9401583a71f4d740636eec44624513126603f121
AzureAD\IonutVilceanu <ionut.vilceanu@empact.app>
2025-07-18 12:49:23 +0300

    change database connection

--- src/pages/api/article.ts
+++ src/pages/api/article.ts
@@ -3,12 +3,12 @@ import { Pool } from 'pg';
 import Parser from 'rss-parser';
 
 const pool = new Pool({
-  user: 'u_bcc35f30_277e_4a3a_9942_e1facdc13637',
-  password: 'bOc6WvA75aor5gMtLm76q051Lz6HEy2Dp7qf6j0qO4O61uvG64a2',
-  host: 'pg.rapidapp.io',
-  port: 5433, // Observă că portul este 5433, nu 5432!
-  database: 'db_bcc35f30_277e_4a3a_9942_e1facdc13637',
-  ssl: { rejectUnauthorized: false },
+  user: 'postgres',
+  password: 'postgres',
+  host: 'icsoft.go.ro',
+  port: 5432, 
+  database: 'newDB',
+  ssl:  false,
   application_name: 'rapidapp_nodejs'
 });
 
