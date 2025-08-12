ec85b759bcac7e41fb2a111d809ea2d68e380481
AzureAD\IonutVilceanu <ionut.vilceanu@empact.app>
2025-08-12 16:14:57 +0300

    add db connection correctly

--- clean-scrape-columns.js
+++ clean-scrape-columns.js
@@ -2,12 +2,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'u_bcc35f30_277e_4a3a_9942_e1facdc13637',
-  password: 'bOc6WvA75aor5gMtLm76q051Lz6HEy2Dp7qf6j0qO4O61uvG64a2',
-  host: 'pg.rapidapp.io',
-  port: 5433,
-  database: 'db_bcc35f30_277e_4a3a_9942_e1facdc13637',
-  ssl: { rejectUnauthorized: false },
+  user: 'postgres',
+  password: 'postgres',
+  host: 'icsoft.go.ro',
+  port: 5432, 
+  database: 'newDB',
+  ssl: false,
   application_name: 'rapidapp_nodejs'
 });
 
